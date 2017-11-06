04c1ff499ffdbb031bc0b208aeda7c87e33159fe
Naji Astier <naji.demolitionman@gmail.com>
2017-11-06 22:46:05 +0100

    Add webpack config

---
+++ app/app.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App/App';
+
+const MOUNT_NODE = document.getElementById('root');
+const render = Component => {
+    ReactDOM.render(
+        <Component />,
+        MOUNT_NODE,
+    )
+};
+
+render(App);
+registerServiceWorker();
+
+if (module.hot) {
+    // Hot reloadable React components and translation json files
+    // modules.hot.accept does not accept dynamic dependencies,
+    // have to be constants at compile-time
+    module.hot.accept(['./components/App/App'], () => {
+        ReactDOM.unmountComponentAtNode(MOUNT_NODE);
+        render(App);
+    });
+}
+
+// Install ServiceWorker and AppCache in the end since
+// it's not most important operation and if main code fails,
+// we do not want it installed
+if (process.env.NODE_ENV === 'production') {
+    require('offline-plugin/runtime').install(); // eslint-disable-line global-require
+}
--- app/components/App/App.js
+++ app/components/App/App.js
@@ -25,6 +25,7 @@ class App extends Component {
         <MobileApp/>
       );
     }
+
     return (
       <div className="App">
         {
